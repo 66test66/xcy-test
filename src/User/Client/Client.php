4c7a6039da25f8d88aab9bf26a0b193c0665b6b9
xingchangyu <892669579@qq.com>
2021-10-14 16:33:01 +0800

    测试包1

---
+++ src/User/Client/Client.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace User\Client;
+
+
+class Client
+{
+    /**
+     * No explanation
+     *
+     * @author Zhiqiang Guo
+     * @return void
+     * @throws Exception
+     * @access public
+     */
+    public function index ()
+    {
+        echo "this is client";
+    }
+
+}
